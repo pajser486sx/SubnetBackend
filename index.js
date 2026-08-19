09598a9abff6fab6833f0f736e8352529c22ef9e
JimTheChickenty <paulavorih9@gmail.com>
2026-08-19 19:17:26 +0200

    Subnet backend start

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+// import dotenv from "dotenv";
+// import { connectDB } from "./db.js";
+
+
+// dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+/*
+app.use(cors({
+  origin: [
+    ""
+  ],
+  credentials: true
+}));
+*/
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Subnet server running!" });
+});
+
+/*
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Frontend can read backend!" });
+});
+*/
+
+
+app.listen(PORT, error => {
+  if (error) {
+    console.error(`Error during server startup: ${error.message}`);
+  } else {
+    console.log(`Subnet server is running at http://localhost:${PORT} !`);
+  }
+});
