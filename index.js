08ca03692d77216ffc948b304c77dda734f20ea0
JimTheChickenty <paulavorih9@gmail.com>
2026-09-05 16:08:19 +0200

    Cors origin change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
   origin: [
-  "http://localhost:5173"
+  "https://thesubnetcalc.netlify.app"
   ],
   credentials: true
 }));
